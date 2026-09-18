d928606c211b325fefad9b69c84cc176662bb8f3
sowmyapaluttla <sowmyapaluttla@gmail.com>
2026-09-18 22:16:53 +0530

    Update 03_case_analysis.sql

--- sales_analysis/03_case_analysis.sql
+++ sales_analysis/03_case_analysis.sql
@@ -1,1 +1,33 @@
+-- Q11. Create a new column “Sale_Type” using CASE:
+ -- High Sale → sale_amount > 30000
+-- Medium Sale → between 15000 and 30000
+-- Low Sale → others.**
+    
+select sale_amount , case 
+when sale_amount > 20000 then 'highsales'
+when sale_amount > 15000 then 'mediumsales'
+else 'lowsales'
+end as sale_type
+from sales ;
 
+-- Q17. Show customer_name, product_name, sale_amount, and a CASE column:
+	-- “Premium” if sale_amount > 40,000
+	-- “Moderate” if (quantity > 5 AND sale_amount BETWEEN 15,000 AND 40,000)
+	-- “Budget” for all other rows
+	-- Only include rows where payment_method is NOT Cash or NOT NULL.
+    
+select customer_name , product_name , sale_amount , case 
+ when sale_amount > 40000 then 'premium' 
+ when quantity > 5 AND sale_amount BETWEEN 15000 AND 40000 then 'moderate' 
+ else 'budget' end as 'sale_'
+ from sales  where payment_method != 'cash' or payment_method != 'null';
+
+-- Q22. Create a CASE-based discount column:
+	-- 10% discount if payment_method = 'UPI'
+	-- 5% discount if payment_method contains the letter 'd'
+	-- 2% discount for all others
+	-- BUT show only records where discount > 5%.
+ 
+select * from (select payment_method , case 
+when payment_method = 'upi' then "10%"
+when payment_method like "%d" then "5%"
